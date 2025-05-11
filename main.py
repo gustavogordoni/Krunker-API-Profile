c90af68579d0b7f22e1653adedc8599497758938
gustavogordoni <gustavogordoni1@gmail.com>
2025-05-11 19:29:01 -0300

    delete index.py and update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from krunker_api import krunker_api
 import json
 
-data = krunker_api("x", debug=True)
+print("Bem-vindo ao KrunkerAPI!")
+print("Por favor, digite o nome do usuário que deseja consultar:")
+username = input()
+
+data = krunker_api(username, debug=True)
 json_string = json.dumps(data)
 print(json_string)
