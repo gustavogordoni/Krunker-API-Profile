c7abaf36c3d9bf2a7b7c1e7191b67b9d4a1baadd
gustavogordoni <gustavogordoni1@gmail.com>
2025-05-11 20:27:27 -0300

    Add func to save json file

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ username = input()
 data = krunker_api(username, debug=True)
 json_string = json.dumps(data)
 print(json_string)
+
+filename = f"data/{username}_profile.json"
+with open(filename, "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
